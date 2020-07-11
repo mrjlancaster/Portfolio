38c33889446a70298bb6235cba6c17351966e123
mrjlancaster <lancast001@gmail.com>
2020-07-10 20:02:37 -0500

    added event handler for home page

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 const projectsPage = document.querySelector('.projects_container');
 const contactPage = document.querySelector('.contact_container');
+const homePage = document.querySelector('.welcome_container');
 
+const homeBtn = document.querySelector('.home');
 const projectsBtn = document.querySelector('.nav-projects');
 const contactBtn = document.querySelector('.nav-contact');
 
+homeBtn.addEventListener('click', function() {
+	homePage.style.transform= 'translateY(0vh)';
+	contactPage.style.transform= 'translateY(-100vh)';
+	projectsPage.style.transform= 'translateX(-100vw)';
+})
+
 projectsBtn.addEventListener('click', function() {
+	contactPage.style.transform= 'translateY(-100vh)';
 	projectsPage.style.transform= 'translateX(0vw)';
 });
 
 contactBtn.addEventListener('click', function() {
+	homePage.style.transform= 'translateY(-100vh)';
+	projectsPage.style.transform= 'translateX(-100vw)';
 	contactPage.style.transform= 'translateY(0vh)';
-})
+});
