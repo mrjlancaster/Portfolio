c784fe09cf7b8644d9b5e073be85a23993f968d0
mrjlancaster <lancast001@gmail.com>
2020-09-11 20:07:33 -0500

    delete old unused code

--- main.js
+++ main.js
@@ -6,26 +6,51 @@ const homeBtn = document.querySelector('.home');
 const projectsBtn = document.querySelector('.nav-projects');
 const contactBtn = document.querySelector('.nav-contact');
 
+
+// home menu button handler
 homeBtn.addEventListener('click', function() {
 	homePage.style.transform= 'translateY(0vh)';
 	contactPage.style.transform= 'translateY(-100vh)';
 	projectsPage.style.transform= 'translateX(-100vw)';
 })
 
+// projects menu button handler
 projectsBtn.addEventListener('click', function() {
-	// setTimeout(() => {
-	// 	homePage.style.transform= 'translateX(-100vw)';
-	// }, 300);
-	projectsPage.style.transform= 'translateX(0vw)';
-	contactPage.style.transform= 'translateY(-100vh)';
+	// hide home page
+	setTimeout(() => {
+		homePage.classList.add('animate__animated', 'animate__fadeOut');
+		
+		setTimeout(() => {
+			homePage.style.display = 'none';
+		}, 1000);
+	}, 1000);
+
+	// display projects page
+	setTimeout(() => {
+		projectsPage.classList.add('animate__animated', 'animate__fadeIn');
+		projectsPage.style.display = 'flex';
+	}, 2500);
 });
 
-contactBtn.addEventListener('click', function() {
-	setTimeout(() => {
-		projectsPage.style.transform= 'translateX(-100vw)';
-	}, 300);
 
-	homePage.style.transform= 'translateY(-100vh)';
-	// projectsPage.style.transform= 'translateX(-100vw)';
-	contactPage.style.transform= 'translateY(0vh)';
+
+// contact menu button handler
+contactBtn.addEventListener('click', function() {
+
+	// hide home page if current
+	if (homePage.style.display == 'flex') {
+		setTimeout(() => {
+			homePage.classList.add('animate__animated', 'animate__fadeOut');
+			
+			setTimeout(() => {
+				homePage.style.display = 'none';
+			}, 1000);
+		}, 1000);
+
+		// display contact page
+		setTimeout(() => {
+			contactPage.classList.add('animate__animated', 'animate__fadeInDown');
+			contactPage.style.display = 'flex';
+		}, 3000);
+	}
 });
