d2b63b13c6619a03ab66f3c4cad16e388fc581aa
mrjlancaster <lancast001@gmail.com>
2020-10-03 21:54:36 -0500

    test props

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -15,7 +15,7 @@ import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box'
 
-const Projects = () => {
+const Projects = (props) => {
 
 	const classes = useStyles();
 
