9f0abeb12b5c421432fb0b9c1ec3d71c64367446
mrjlancaster <lancast001@gmail.com>
2020-10-11 00:14:39 -0500

    delete unused code and empty lines

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,30 +1,22 @@
 import React from 'react';
 import Cards from './Cards';
 
-// images import
-import Trans1go from '../img/trans1go.jpg';
-import am from '../img/am.jpg';
-import padaOne from '../img/padaone.jpg';
-import WorldClock from '../img/world_clock.jpg';
-import ArcadeGame from '../img/arcade_game.jpg';
-
 // Material UI imports
 import useStyles from '../styles';
 import { Typography } from '@material-ui/core';
 import Box from '@material-ui/core/Box'
 
 const Projects = () => {
-
 	const classes = useStyles();
 
-
 	return (
-		<Box className={classes.projectsBox}>
+		<Box>
 			<Typography variant="h2" className={classes.projectsHeading}>
 				Projects
 			</Typography>
-				<Cards />
-		// </Box>
+			<Cards />
+		</Box>
+		
 
 	)
 }
