159911243337a6a8ae9196c3eedea08bdb20b871
mrjlancaster <lancast001@gmail.com>
2020-10-12 18:09:48 -0500

    run build

---
+++ my-portfolio/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
+
+// Create express app
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'my-portfolio', 'public')));
+
+// Route
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+// listen to port
+app.listen(PORT, () => {
+	console.log('Listening on port 3000');
+})
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -4,14 +4,11 @@ import './index.css';
 import App from './App';
 import theme from './theme';
 import { ThemeProvider } from '@material-ui/core';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-	(<BrowserRouter>
-		<ThemeProvider theme={theme}>
-			<App />
-		</ThemeProvider>
-	</BrowserRouter>)
+	<ThemeProvider theme={theme}>
+		<App />
+	</ThemeProvider>
 	,
   document.getElementById('root')
 );	
