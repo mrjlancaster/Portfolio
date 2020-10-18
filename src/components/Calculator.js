6cd54f5dc088c4aaf0380861b7aa3b0c2d8faee9
mrjlancaster <lancast001@gmail.com>
2020-10-18 17:21:10 -0500

    calculator component

---
+++ src/components/Calculator.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Calculator = () => {
+	return (
+		<div>
+			<div>
+				<img src="" />
+			</div>
+			<div>
+				<img src="" />
+			</div>
+		</div>
+	)
+}
+
+export default Calculator;
