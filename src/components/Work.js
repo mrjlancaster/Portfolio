51c239993163393b239eb1d91210dc7c238fe15f
mrjlancaster <lancast001@gmail.com>
2020-10-22 12:01:53 -0500

    add list to one component

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Work.css';
 
-import List from './cards/List';
 import AM2 from './cards/AM2';
 import Padaone2 from './cards/Padaone2';
 import Trans1go2 from './cards/Trans1go2';
 import ArcadeGame2 from './cards/ArcadeGame2';
 import WorldClock2 from './cards/WorldClock2';
 
+// material ui imports
+import { Typography } from '@material-ui/core';
+
 const Work = () => {
+	const [isShown, setIsShown] = useState(false);
+
 	return (
 		<div className="work">
-			<List />
-			<WorldClock2 />
+			<Typography className="work_heading" variant="h2">
+				Some of my work
+			</Typography>
+			<div className="work_container">
+				<div className="project_list-container">
+					<ul className="projects_ul">
+						<li className="projects-li project--active">Trans1go.com</li>
+						<li className="projects-li">Padaone Inc</li>
+						<li className="projects-li">AM Architecture & Design</li>
+						<li className="projects-li">World Clock (Web App)</li>
+						<li className="projects-li">Arcade game</li>
+					</ul>
+				</div>
+				<div className="projects">
+					<Trans1go2 />
+					<Padaone2 />
+					<AM2 />
+					<WorldClock2 />
+					<ArcadeGame2 />
+				</div>
+			</div>
 		</div>
 
 	)
