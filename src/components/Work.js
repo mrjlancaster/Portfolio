8b907cef546b974ef1f4c58fff7d698af8fef849
mrjlancaster <lancast001@gmail.com>
2020-10-22 23:21:24 -0500

    delete unused code

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,11 +2,7 @@ import React, { useState } from 'react';
 import './Work.css';
 import './cards/AM2.css'
 
-import AM2 from './cards/AM2';
-import Padaone2 from './cards/Padaone2';
 import Trans1go2 from './cards/Trans1go2';
-import ArcadeGame2 from './cards/ArcadeGame2';
-import WorldClock2 from './cards/WorldClock2';
 
 // import screenshots
 import Trans1goScreenshot from '../img/trans1go.jpg';
