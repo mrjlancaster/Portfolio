798676271dc34877bdbf840e37f4db3c80d67b93
mrjlancaster <lancast001@gmail.com>
2020-11-01 20:09:43 -0600

    change background color

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ const About = () => {
 					I'm a Frontend Engineer passionate about building innovative web applications and modern responsive web designs. My main focus as a developer is in HTML, CSS, JavaScript, and ReactJS. Throughout my time as a frontend engineer I've learned several technologies and have had some brief experiences with different modern frameworks and languages. I'm a quick learner, and a self-taught programmer. I'm able to learn any new technology very quickly and effectively, being able to apply to projects as needed.
 					<br />
 					<br />
-					In my personal time I like to read, code and practice my programming skills, create personal projects, play video games (of course), and find competitive games/hobbies to participate in, such as Men's Physique contests, casual chess. I love doing outdoors activities and playing sports such as basketball. Traveling and seeing new places and exploring the outside world is also a big interest that takes place in my life. The older I get the more I find opportunities to go on adventures.
+					In my personal time I like to read, code and practice my programming skills, create personal projects, play video games (of course), and find competitive games/hobbies to participate in, such as Men's Physique contests, and casual chess. I love doing outdoors activities and playing sports such as basketball. Traveling and seeing new places and exploring the outside world is also a big interest that takes place in my life. The older I get the more I find opportunities to go on new adventures.
 				</Typography>
 			</div>
 		</div>
