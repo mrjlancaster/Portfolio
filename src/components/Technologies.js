2d079f4c7e2e7bb87b0da8495fb5e8c28a6a5372
mrjlancaster <lancast001@gmail.com>
2020-11-02 22:44:50 -0600

    add media query to tech icons

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -18,32 +18,32 @@ const Technologies = () => {
 		
 			<div className='grid'>
 				<div className="icon_wrapper">
-					<Typography variant="h6">HTML5</Typography>
+					<h6 className="icon_titles">HTML5</h6>
 					<i className="fab fa-html5" style={{color: '#ff5a44'}}></i>
 				</div>
 
 				<div className="icon_wrapper">
-					<Typography variant="h6">CSS3</Typography>
+					<h6 className="icon_titles">CSS3</h6>
 					<i className="fab fa-css3-alt" style={{color: '#0a7ae9'}}></i>
 				</div>
 
 				<div className="icon_wrapper">
-					<Typography variant="h6">Sass</Typography>
+					<h6 className="icon_titles">Sass</h6>
 					<i className="fab fa-sass" style={{color: '#e759b8'}}></i>
 				</div>
 
 				<div className="icon_wrapper">
-					<Typography variant="h6">JavaScript</Typography>
+					<h6 className="icon_titles">JavaScript</h6>
 					<i className="fab fa-js-square" style={{color: '#ffd53d'}}></i>
 				</div>
 
 				<div className="icon_wrapper">
-					<Typography variant="h6">React</Typography>
+					<h6 className="icon_titles">React</h6>
 					<i className="fab fa-react" style={{color: '#73d3ff'}}></i>
 				</div>
 
 				<div className="icon_wrapper">
-					<Typography variant="h6">NodeJS</Typography>
+					<h6 className="icon_titles">NodeJS</h6>
 					<i className="fab fa-node-js" style={{color: '#1f9e54'}}></i>
 				</div>
 			</div>
