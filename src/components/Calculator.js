7063bf46012ae0485716c7288477852e7592c0b9
mrjlancaster <lancast001@gmail.com>
2020-11-05 09:38:41 -0600

    add div

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -19,19 +19,25 @@ const Calculator = () => {
 			<p className="calc-description">Visit here: <a href="https://mrjlancaster.github.io/calculator-darkmode/" target="blank">https://mrjlancaster.github.io/calculator-darkmode/</a></p>
 		
 			<div className='leftBox'>
-				<h2 className="calc_mode-heading">
-					Light <br />
-					Mode
-				</h2>
-				<img src={LightMode} alt="calculator" className='lightCalc' />
+				<div className="calculator_heading1-container">
+					<h2 className="calc_mode-heading">
+						Light <br />
+						Mode
+					</h2>
+				</div>
+				<div className="blue_calculator-container">
+					<img src={LightMode} alt="calculator" className='blue_calculator' />
+				</div>
 			</div>
 			<div className='rightBox'>
-				<img src={DarkMode} alt="calculator" className='calc' />
 				<div>
-				<h2 className="calc_mode-heading">Dark Mode</h2>
-				<p className='darkModeText'>
-					The technologies I used here were <b>HTML5, CSS3, JavaScript, and Photoshop.</b> A lot of <span className={classes.ifStatements}>if statements</span> to get this done. Very straight forward.
-				</p>
+					<img src={DarkMode} alt="calculator" className='calc' />
+				</div>
+				<div>
+					<h2 className="calc_mode-heading">Dark Mode</h2>
+					<p className='darkModeText'>
+						The technologies I used here were <b>HTML5, CSS3, JavaScript, and Photoshop.</b> A lot of <span className={classes.ifStatements}>if statements</span> to get this done. Very straight forward.
+					</p>
 				</div>
 				
 			</div>
