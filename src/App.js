034e16a3510e84b81a8d6080d53c9af3a2309896
mrjlancaster <lancast001@gmail.com>
2020-11-26 14:11:10 -0600

    delete unused old components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+
+// Components imports
 import Header from './components/Header';
 import Welcome from './components/Welcome';
-import Contact from './components/Contact';
-import Calculator from './components/Calculator';
-import Technologies from './components/Technologies';
 import About from './components/About';
 import Work from './components/Work';
+import Calculator from './components/Calculator';
+import Technologies from './components/Technologies';
+import Contact from './components/Contact';
 
 const App = () => {
   return (
