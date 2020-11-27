d60ab87fbc5e2c62442fb65104f7e5dfacf39dfc
mrjlancaster <lancast001@gmail.com>
2020-11-26 19:02:37 -0600

    Updates

---
+++ precache-manifest.046f7c1414c6416dbbf7cd930e495bb0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ced61223bb75649eef81090892c4721",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ee80a25fdea86030f872",
+    "url": "/Portfolio/static/css/main.1007a76f.chunk.css"
+  },
+  {
+    "revision": "7ceb8b8c37f4d7c30ab9",
+    "url": "/Portfolio/static/js/2.3baed272.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.3baed272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee80a25fdea86030f872",
+    "url": "/Portfolio/static/js/main.d2435149.chunk.js"
+  },
+  {
+    "revision": "627bdb01d220a474fd8b",
+    "url": "/Portfolio/static/js/runtime-main.9e287cfb.js"
+  },
+  {
+    "revision": "4aaec4f5f36623c29bb13f71ce8c9386",
+    "url": "/Portfolio/static/media/am.4aaec4f5.jpg"
+  },
+  {
+    "revision": "b7be904b6a07bea1478c0d472b9a04fb",
+    "url": "/Portfolio/static/media/arcade_game.b7be904b.jpg"
+  },
+  {
+    "revision": "a7c0379aa31fbc0eb0c31febde9933d1",
+    "url": "/Portfolio/static/media/blue-calculator.a7c0379a.png"
+  },
+  {
+    "revision": "39643b9f604e4885691c6fd7b09d908b",
+    "url": "/Portfolio/static/media/dark-calculator.39643b9f.png"
+  },
+  {
+    "revision": "5dd2cd5ede38f6625b3e148e8df172c2",
+    "url": "/Portfolio/static/media/padaone.5dd2cd5e.jpg"
+  },
+  {
+    "revision": "3d49cdef332129974570581cb1dd23b6",
+    "url": "/Portfolio/static/media/profile_pic.3d49cdef.jpg"
+  },
+  {
+    "revision": "054ec156a568fab5db2ba21b6d82bb6b",
+    "url": "/Portfolio/static/media/trans1go.054ec156.jpg"
+  },
+  {
+    "revision": "46e2f022a74814722b3e4c22400c06ac",
+    "url": "/Portfolio/static/media/world_clock.46e2f022.jpg"
+  }
+]);
