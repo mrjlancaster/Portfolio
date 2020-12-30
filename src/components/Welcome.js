83fdadf1103421961288a2a97f2136e0c9c81575
mrjlancaster <lancast001@gmail.com>
2020-12-30 14:18:55 -0600

    remove unused state hook

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -8,7 +8,6 @@ import Box from '@material-ui/core/Box';
 function Welcome() {
 	const classes = useStyles();
 
-	const [nav, setNav] = useState({opacity: '0'})
 	const [intro, setIntro] = useState({opacity: '0'});
 	const [isShown,setIsShown] = useState({opacity: '0'})
 	
@@ -19,12 +18,6 @@ function Welcome() {
 				transition: '1100ms'
 			})
 		}, 500);
-		setTimeout(() => {
-			setNav({
-				opacity: '1',
-				transition: '1200ms'
-			})
-		}, 2300);
 		setTimeout(() => {
 			setIsShown({
 				opacity: '1',
