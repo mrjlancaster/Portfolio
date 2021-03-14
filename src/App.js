a4a3bcaf371f6dbf33d1d9ada3790e1f1a776b1f
mrjlancaster <lancast001@gmail.com>
2021-03-13 20:08:30 -0600

    add icons and change section position

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 // Components imports
-import Header from './components/Header';
-import Welcome from './components/Welcome';
-import About from './components/About';
-import Work from './components/Work';
-import Calculator from './components/Calculator';
-import Technologies from './components/Technologies';
-import Contact from './components/Contact';
+import Header from "./components/Header";
+import Welcome from "./components/Welcome";
+import About from "./components/About";
+import Work from "./components/Work";
+import Calculator from "./components/Calculator";
+import Technologies from "./components/Technologies";
+import Contact from "./components/Contact";
 
 const App = () => {
-  return (
+	return (
 		<div className="App">
 			<Header />
 			<Welcome />
 			<About />
+			<Technologies />
 			<Work />
 			<Calculator />
-			<Technologies />
 			<Contact />
 		</div>
-  );
-}
+	);
+};
 
 export default App;
--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,52 +1,64 @@
-import React from 'react';
-import './Technologies.css';
+import React from "react";
+import "./Technologies.css";
+import { FaReact } from "react-icons/fa";
+import { FaJs } from "react-icons/fa";
+import { FaHtml5 } from "react-icons/fa";
+import { FaCss3 } from "react-icons/fa";
+import { FaSass } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa";
+import { FaGitAlt } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { DiMongodb } from "react-icons/di";
+import { DiResponsive } from "react-icons/di";
+import { DiAws } from "react-icons/di";
 
 // material ui imports
-import { Box } from '@material-ui/core';
+import { Box } from "@material-ui/core";
 
 const Technologies = () => {
-
 	return (
 		<div id="my stack" className="tech_container">
 			<Box className="typography_container">
 				<h4 className="stack_title">My stack</h4>
-				<p className="stack_text">Day in and day out I'm utilizing these technologies to build websites and web applications</p>
+				<p className="stack_text">
+					Day in and day out I'm utilizing these technologies to build
+					websites and web applications
+				</p>
 			</Box>
-		
-			<div className='grid'>
+
+			<div className="grid">
 				<div className="icon_wrapper">
 					<h6 className="icon_titles">HTML5</h6>
-					<i className="fab fa-html5" style={{color: '#ff5a44'}}></i>
+					<i className="fab fa-html5" style={{ color: "#ff5a44" }}></i>
 				</div>
 
 				<div className="icon_wrapper">
 					<h6 className="icon_titles">CSS3</h6>
-					<i className="fab fa-css3-alt" style={{color: '#0a7ae9'}}></i>
+					<i className="fab fa-css3-alt" style={{ color: "#0a7ae9" }}></i>
 				</div>
 
 				<div className="icon_wrapper">
 					<h6 className="icon_titles">Sass</h6>
-					<i className="fab fa-sass" style={{color: '#e759b8'}}></i>
+					<i className="fab fa-sass" style={{ color: "#e759b8" }}></i>
 				</div>
 
 				<div className="icon_wrapper">
 					<h6 className="icon_titles">JavaScript</h6>
-					<i className="fab fa-js-square" style={{color: '#ffd53d'}}></i>
+					<i className="fab fa-js-square" style={{ color: "#ffd53d" }}></i>
 				</div>
 
 				<div className="icon_wrapper">
 					<h6 className="icon_titles">React</h6>
-					<i className="fab fa-react" style={{color: '#73d3ff'}}></i>
+					<i className="fab fa-react" style={{ color: "#73d3ff" }}></i>
 				</div>
 
 				<div className="icon_wrapper">
 					<h6 className="icon_titles">NodeJS</h6>
-					<i className="fab fa-node-js" style={{color: '#1f9e54'}}></i>
+					<i className="fab fa-node-js" style={{ color: "#1f9e54" }}></i>
 				</div>
 			</div>
-
-		</div> 
-	)
-}
+		</div>
+	);
+};
 
 export default Technologies;
