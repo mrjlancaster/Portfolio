bba5d573e737834f87260cffc726f61860e306e6
Jonathan Lancaster <lancast001@gmail.com>
2023-12-11 19:35:41 -0600

    update code and add new project info

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,52 +1,59 @@
-import React from 'react';
-import './Technologies.css';
+import React from "react";
+import "./Technologies.css";
 
 // material ui imports
-import { Box } from '@material-ui/core';
+import { Box } from "@material-ui/core";
+
+const tech = [
+	{
+		title: "HTML5",
+		icon: <i className="fab fa-html5" style={{ color: "#ff5a44" }}></i>,
+	},
+	{
+		title: "CSS3",
+		icon: <i className="fab fa-css3-alt" style={{ color: "#0a7ae9" }}></i>,
+	},
+	{
+		title: "Sass",
+		icon: <i className="fab fa-sass" style={{ color: "#e759b8" }}></i>,
+	},
+	{
+		title: "JavaScript",
+		icon: <i className="fab fa-js-square" style={{ color: "#ffd53d" }}></i>,
+	},
+	{
+		title: "React.js",
+		icon: <i className="fab fa-react" style={{ color: "#73d3ff" }}></i>,
+	},
+	{
+		title: "NodeJS",
+		icon: <i className="fab fa-node-js" style={{ color: "#1f9e54" }}></i>,
+	},
+];
 
 const Technologies = () => {
-
 	return (
 		<div id="my stack" className="tech_container">
 			<Box className="typography_container">
 				<h4 className="stack_title">My stack</h4>
-				<p className="stack_text">Day in and day out I'm utilizing these technologies to build websites and web applications</p>
+				<p className="stack_text">
+					Day in and day out I'm utilizing these technologies to build
+					websites and web applications
+				</p>
 			</Box>
-		
-			<div className='grid'>
-				<div className="icon_wrapper">
-					<h6 className="icon_titles">HTML5</h6>
-					<i className="fab fa-html5" style={{color: '#ff5a44'}}></i>
-				</div>
 
-				<div className="icon_wrapper">
-					<h6 className="icon_titles">CSS3</h6>
-					<i className="fab fa-css3-alt" style={{color: '#0a7ae9'}}></i>
-				</div>
-
-				<div className="icon_wrapper">
-					<h6 className="icon_titles">Sass</h6>
-					<i className="fab fa-sass" style={{color: '#e759b8'}}></i>
-				</div>
-
-				<div className="icon_wrapper">
-					<h6 className="icon_titles">JavaScript</h6>
-					<i className="fab fa-js-square" style={{color: '#ffd53d'}}></i>
-				</div>
-
-				<div className="icon_wrapper">
-					<h6 className="icon_titles">React</h6>
-					<i className="fab fa-react" style={{color: '#73d3ff'}}></i>
-				</div>
-
-				<div className="icon_wrapper">
-					<h6 className="icon_titles">NodeJS</h6>
-					<i className="fab fa-node-js" style={{color: '#1f9e54'}}></i>
-				</div>
+			<div className="grid">
+				{tech.map((item, i) => {
+					return (
+						<div key={i} className="icon_wrapper">
+							<h6 className="icon_titles">{item.title}</h6>
+							{item.icon}
+						</div>
+					);
+				})}
 			</div>
-
-		</div> 
-	)
-}
+		</div>
+	);
+};
 
 export default Technologies;
--- src/components/Work.js
+++ src/components/Work.js
@@ -15,71 +15,63 @@ import ArcadeGameScreenshot from "../img/arcade_game.jpg";
 import { Typography } from "@material-ui/core";
 
 const Work = () => {
-	const [li, setLi] = useState("trans1go.com");
-	const [project, setProject] = useState({
-		image: Trans1goScreenshot,
-		title: "Trans1go.com",
-		description: `Trans1go.com is an auto repair shop focused on delivering mobile repairs (service at your door). Responsive website, mobile and tablet friendly. Front-end and back-end technologies were used in this project, such as HTML5, CSS3/Sass, Javascript, nodeJS, and REST APIs.`,
-		link: "http://trans1go.com/",
-	});
+	const [activeTab, setActiveTab] = useState(0);
 
-	const switchLi = (e) => {
-		const key = e.target;
-		if (key.matches("li")) {
-			if (key.innerText === "Trans1go.com") {
-				setLi("trans1go.com");
-				setProject({
-					image: Trans1goScreenshot,
-					title: "Trans1go.com",
-					description: `Trans1go.com is an auto repair shop focused on delivering mobile repairs (service at your door). Responsive website, mobile and tablet friendly. Front-end and back-end technologies were used in this project, such as HTML5, CSS3/Sass, Javascript, nodeJS, and REST APIs.`,
-					link: "http://trans1go.com/",
-				});
-			}
+	const tabs = [
+		"Trans1go.com",
+		"Padaone Inc.",
+		"AM Architecture & Design",
+		"World Clock (Web App)",
+		"Arcade game",
+		"Tic Tac Toe",
+	];
 
-			if (key.innerText === "Padaone Inc") {
-				setLi("padaone");
-				setProject({
-					image: PadaoneScreenshot,
-					title: "Padaone Inc",
-					description:
-						"Padaone is a marketplace for individuals to take classes and train with coaches all over the world. I was responsible to build the first company's Landing Page. Technologies used in this project were HTML5, CSS3/Sass, and Javascript.",
-					link: "https://www.padaone.app/",
-				});
-			}
+	const tabContent = [
+		{
+			image: Trans1goScreenshot,
+			title: "Trans1go.com",
+			description: `Trans1go.com is an auto repair shop focused on delivering mobile repairs (service at your door). Responsive website, mobile and tablet friendly. Front-end and back-end technologies were used in this project, such as HTML5, CSS3/Sass, Javascript, nodeJS, and REST APIs.`,
+			link: "http://trans1go.com/",
+		},
+		{
+			image: PadaoneScreenshot,
+			title: "Padaone Inc",
+			description:
+				"Padaone is a marketplace for individuals to take classes and train with coaches all over the world. I was responsible to build the first company's Landing Page. Technologies used in this project were HTML5, CSS3/Sass, and Javascript.",
+			link: "https://www.padaone.app/",
+		},
+		{
+			image: AMScreenshot,
+			title: "AM Architecture & Design",
+			description:
+				"This is a mockup website I've built based on a design I found as an inspiration. AM Architecture & Design was built utilizing HTML5 and CSS3. Simple, elegant and great design.",
+			link: "https://mrjlancaster.github.io/Responsive-website/",
+		},
+		{
+			image: WorldClockScreenshot,
+			title: "World clock web application",
+			description:
+				"Simple digital world clock web application built in HTML5,CSS3, and JavaScript.",
+			link: "https://mrjlancaster.github.io/world_clock/",
+		},
+		{
+			image: ArcadeGameScreenshot,
+			title: "Arcade Game",
+			description:
+				"I've used my Javascript skills to build an Arcade game. Just for the fun also for my daily coding practices. Check it out!",
+			link: "https://mrjlancaster.github.io/Arcade-Game/",
+		},
+		{
+			image: ArcadeGameScreenshot,
+			title: "Tic Tac Toe",
+			description: "",
+			link: "https://github.com/mrjlancaster/tic-tac-toe",
+		},
+	];
 
-			if (key.innerText === "AM Architecture & Design") {
-				setLi("am");
-				setProject({
-					image: AMScreenshot,
-					title: "AM Architecture & Design",
-					description:
-						"This is a mockup website I've built based on a design I found as an inspiration. AM Architecture & Design was built utilizing HTML5 and CSS3. Simple, elegant and great design.",
-					link: "https://mrjlancaster.github.io/Responsive-website/",
-				});
-			}
-
-			if (key.innerText === "World Clock (Web App)") {
-				setLi("worldclock");
-				setProject({
-					image: WorldClockScreenshot,
-					title: "World clock web application",
-					description:
-						"Simple digital world clock web application built in HTML5,CSS3, and JavaScript.",
-					link: "https://mrjlancaster.github.io/world_clock/",
-				});
-			}
-
-			if (key.innerText === "Arcade game") {
-				setLi("arcade game");
-				setProject({
-					image: ArcadeGameScreenshot,
-					title: "Arcade Game",
-					description:
-						"I've used my Javascript skills to build an Arcade game. Just for the fun also for my daily coding practices. Check it out!",
-					link: "https://mrjlancaster.github.io/Arcade-Game/",
-				});
-			}
-		}
+	const handleTab = (e) => {
+		const index = e.target.id;
+		setActiveTab(Number(index));
 	};
 
 	return (
@@ -89,42 +81,22 @@ const Work = () => {
 			</Typography>
 			<div className="work_container">
 				<div className="project_list-container">
-					<ul onClick={switchLi}className="projects_ul">
-						<li
-							className={
-								li === "trans1go.com"
-									? "project--active"
-									: "projects-li"
-							}
-						>
-							Trans1go.com
-						</li>
-						<li
-							className={
-								li === "padaone" ? "project--active" : "projects-li"
-							}
-						>
-							Padaone Inc
-						</li>
-						<li
-							className={li === "am" ? "project--active" : "projects-li"}
-						>
-							AM Architecture & Design
-						</li>
-						<li
-							className={
-								li === "worldclock" ? "project--active" : "projects-li"
-							}
-						>
-							World Clock (Web App)
-						</li>
-						<li
-							className={
-								li === "arcade game" ? "project--active" : "projects-li"
-							}
-						>
-							Arcade game
-						</li>
+					<ul onClick={handleTab} className="projects_ul">
+						{tabs.map((tab, i) => {
+							return (
+								<li
+									key={i}
+									id={i}
+									className={
+										activeTab === i
+											? "project--active"
+											: "projects-li"
+									}
+								>
+									{tab}
+								</li>
+							);
+						})}
 					</ul>
 				</div>
 
@@ -132,22 +104,20 @@ const Work = () => {
 					<div className="project_details">
 						<div className="project-container">
 							<img
-								src={project.image}
+								src={tabContent[activeTab].image}
 								className="pic"
-								alt="trans1go image"
+								alt="project pic"
 							/>
 							<div className="project_text-wrapper">
 								<h5 className="project_title-name">
-									{project.title}
+									{tabContent[activeTab].title}
 								</h5>
-								<p
-									className="project-description"
-								>
-									{project.description}
+								<p className="project-description">
+									{tabContent[activeTab].description}
 								</p>
 								<div>
 									<a
-										href={project.link}
+										href={tabContent[activeTab].link}
 										target="blank"
 										className="projectBtn"
 									>
